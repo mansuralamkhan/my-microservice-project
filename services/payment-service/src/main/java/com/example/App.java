0b6806d18c95aa451dea0e17d3bc0829131dd5e8
mansuralamkhan <mansuralam44@gmail.com>
2026-08-07 18:27:08 +0530

    Update App.java

--- services/payment-service/src/main/java/com/example/App.java
+++ services/payment-service/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hellddo from Docker!");
+        System.out.println("Hellddo from ddDocker!");
     }
 }
