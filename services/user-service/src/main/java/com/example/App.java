f5be798a42c3a4d20f3283801e6bb6881d04d40a
mansuralamkhan <mansuralam44@gmail.com>
2026-08-08 23:56:10 +0530

    Update App.java

--- services/user-service/src/main/java/com/example/App.java
+++ services/user-service/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("ss froddm Docker!");
+        System.out.println("ss froddm,, Docker!");
     }
 }
