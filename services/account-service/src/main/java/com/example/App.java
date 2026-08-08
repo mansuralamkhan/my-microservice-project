b953f7be541b4eb34bc8c162c772529268e9bd29
mansuralamkhan <mansuralam44@gmail.com>
2026-08-08 19:04:28 +0530

    Updated three services

--- services/account-service/src/main/java/com/example/App.java
+++ services/account-service/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hellddo ddkhsfsroam Docker!");
+        System.out.println("Helvvlddo ddkhsddfsroam Docker!");
     }
 }
