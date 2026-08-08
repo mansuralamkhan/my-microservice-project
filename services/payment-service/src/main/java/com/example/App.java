9ae61df041991a418f66e80c726c529570c58573
mansuralamkhan <mansuralam44@gmail.com>
2026-08-08 14:24:01 +0530

    Update App.java

--- services/payment-service/src/main/java/com/example/App.java
+++ services/payment-service/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Helldddo dbfrd,domm,mm.m ,m,, ,mdd..,Docker!");
+        System.out.println("Helldddo dbfrd,domm,m,m.m ,m,, ,mdd..,Docker!");
     }
 }
