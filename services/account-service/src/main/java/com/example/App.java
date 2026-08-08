7804f02f0bbb531c9fb1b3130dc4519242db5a97
mansuralamkhan <mansuralam44@gmail.com>
2026-08-08 19:49:53 +0530

    Fix typo in Docker greeting message

--- services/account-service/src/main/java/com/example/App.java
+++ services/account-service/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Helvvlddo ddkhsddfsroam Docker!");
+        System.out.println("Helvvlddo ddk.hsddfsroam Docker!");
     }
 }
