24fa63a267545d97c3869ff5853171e62bdb4e3e
mansuralamkhan <mansuralam44@gmail.com>
2026-08-09 11:06:25 +0530

    Update App.java

--- services/user-service/src/main/java/com/example/App.java
+++ services/user-service/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("ss froddmmm....m,, Dockmmer!");
+        System.out.println("ss froddmmmm....m,, Dockmmer!");
     }
 }
