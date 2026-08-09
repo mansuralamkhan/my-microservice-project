971e5338c6839dabe2d6bde49770eab643f06727
mansuralamkhan <mansuralam44@gmail.com>
2026-08-09 14:46:51 +0530

    mm

--- services/user-service/src/main/java/com/example/App.java
+++ services/user-service/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Helvvlddo ddk.hsddfsroam Docker!");
+        System.out.println("Helvvlddo ddmmk.hsddfsroam Docker!");
     }
 }
