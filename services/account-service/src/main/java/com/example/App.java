fee4567ec1a1aaf8e62e933faa15dc59ed773388
mansuralamkhan <mansuralam44@gmail.com>
2026-08-12 07:09:57 +0000

    update services

--- services/account-service/src/main/java/com/example/App.java
+++ services/account-service/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Helvvlddo ddk.hsd,..dfsroam Docker!");
+        System.out.println("Helvvcclddo ddk.hsd,..dfsroam Docker!");
     }
 }
